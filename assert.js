30eed42947590738b4b9289313b20e2b46a84457
Aye Erumi <ogharaerumiaye@gmail.com>
2023-05-12 22:03:48 +0100

    Edited comment

--- assert.js
+++ assert.js
@@ -4,7 +4,7 @@ Description: It provides a set of assertion functions for writing tests
 and performing runtime assertions. These functions are used to check if certain 
 conditions are met and throw errors if the conditions are not satisfied. 
 The "assert" module helps with debugging and verifying the correctness of your code.
-
+ 
 */
 
 const assert = require('assert');
